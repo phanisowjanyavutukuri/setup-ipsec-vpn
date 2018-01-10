e7aa69c3c2a58bcd50e67c31824a42922ca5b9a8
phanisowjanyavutukuri <32537760+phanisowjanyavutukuri@users.noreply.github.com>
2018-01-10 09:48:14 +0000

    Update vpnsetup.sh

--- vpnsetup.sh
+++ vpnsetup.sh
@@ -24,9 +24,9 @@
 # - All values MUST be placed inside 'single quotes'
 # - DO NOT use these special characters within values: \ " '
 
-YOUR_IPSEC_PSK=''
-YOUR_USERNAME=''
-YOUR_PASSWORD=''
+YOUR_IPSEC_PSK='myl2tpkey'
+YOUR_USERNAME='vpn'
+YOUR_PASSWORD='hyderabad'
 
 # Important notes:   https://git.io/vpnnotes
 # Setup VPN clients: https://git.io/vpnclients
